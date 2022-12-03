a1be95bc920177373882392298b8e43f40058325
Neeraj Verma <72142904+neeraj-verma-20@users.noreply.github.com>
2022-12-03 10:24:49 +0530

    Class Components to Function Component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,148 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoadingBar from 'react-top-loading-bar'
+import LoadingBar from "react-top-loading-bar";
 
+const App = () => {
+  const apiKey = process.env.REACT_APP_NEWS_API;
+  const country = "in";
+  const pageSize = 6;
+  const headlineType = "top-headlines";
 
-export default class App extends Component {
-  pageSize = 6;
-  apiKey = process.env.REACT_APP_NEWS_API;
+  const [progress, setProgress] = useState(0);
 
-  state = {
-    progress: 0
-  }
-
-  setProgress = (progress) => {
-    this.setState({ progress: progress })
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <NavBar />
-          <LoadingBar
-            height={3}
-            color='#f11946'
-            progress={this.state.progress}
+  return (
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <LoadingBar height={3} color="#f11946" progress={progress} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="home"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="general"
+              />
+            }
           />
-          <Routes>
-            <Route exact path="/" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="home" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="general" />} />
-            <Route exact path="/business" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="business" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="business" />} />
-            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="entertainment" />} />
-            <Route exact path="/general" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="general" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="general" />} />
-            <Route exact path="/health" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="health" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="health" />} />
-            <Route exact path="/science" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="science" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="science" />} />
-            <Route exact path="/sports" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="sports" />} />
-            <Route exact path="/technology" element={<News apiKey={this.apiKey} setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country={this.country} headlineType={this.headlineType} category="technology" />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="business"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="business"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="entertainment"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="entertainment"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/general"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="general"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="general"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="health"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="health"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="science"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="science"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="sports"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="sports"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                apiKey={apiKey}
+                setProgress={setProgress}
+                key="technology"
+                pageSize={pageSize}
+                country={country}
+                headlineType={headlineType}
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,52 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger">{source}</span>
+        </div>
+
+        <img
+          src={
+            !imageUrl ? "https://images.barrons.com/im-677120/social" : imageUrl
+          }
+          className="card-img-top"
+          alt="article-img"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "Unknow" : author} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+            rel="noreferrer"
           >
-            <span className="badge rounded-pill bg-danger">{source}</span>
-          </div>
-
-          <img
-            src={
-              !imageUrl
-                ? "https://images.barrons.com/im-677120/social"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="article-img"
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "Unknow" : author} on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-              rel="noreferrer"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default NewsItem;
